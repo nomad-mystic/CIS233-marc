c27c568b7252313dadbc83639439d9550af5f636
nomadMystic <nomadmystics@gmail.com>
2016-01-13 10:24:35 -0800

    This is the 5th commit and push to remote for CIS223Marc. I started working on the week 4 tutorial videos. We are adding a bookmark to chrome for WCAG program.

---
+++ weekFour/bookMark.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Nomad_Mystic on 1/11/2016.
+ */
+
+
+javascript:(
+    void(
+        function() {
+            var fileRef;
+            var loaded = false;
+
+            try {
+                loaded = __WCAG_LOADED__;
+            }
+            catch(err) {
+            }
+
+            if (!loaded) {
+                fileRef = document.createElement('link');
+                fileRef.rel = 'stylesheet';
+                fileRef.type = 'text/css';
+                fileRef.href = 'http://localhost:8080/CIS233Marc/weekFour/wcag.css';
+                document.getElementsByTagName('head')[0].appendChild(fileRef);
+
+                fileRef = document.createElement('script');
+                fileRef.src = "http://localhost:8080/CIS233Marc/weekFour/wcag.js";
+                fileRef.type = 'text/javascript';
+                document.getElementsByTagName('head')[0].appendChild(fileRef);
+            } else {
+                checkDocumentWCAG();
+            }
+        }()
+    )
+);
+
---
+++ weekFour/wcag.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nomad_Mystic on 1/11/2016.
+ */
+
+
+
+function checkDocumentWCAG() {
+
+    console.log('Hello World');
+
+}
+checkDocumentWCAG();
+var __WCAG_LOADED__ = true;
